aa0b46412466855aebcc682772be7a0dc5baa2df
Noah Davis <noahd1@yahoo.com>
2014-04-17 19:56:07 -0400

    Exit early and notify if no CODECLIMATE_REPO_TOKEN found

--- bin/codeclimate.js
+++ bin/codeclimate.js
@@ -12,6 +12,13 @@ process.stdin.on("data", function(chunk) {
     input += chunk;
 });
 
+var repo_token = process.env.CODECLIMATE_REPO_TOKEN;
+
+if(repo_token == undefined || repo_token.trim() == "") {
+  console.error("No CODECLIMATE_REPO_TOKEN found. A CODECLIMATE_REPO_TOKEN must be specified as an environment variable.");
+  process.exit(1);
+}
+
 process.stdin.on("end", function() {
   formatter = new Formatter()
   formatter.format(input, function(err, json) {
@@ -21,7 +28,7 @@ process.stdin.on("end", function() {
       if (process.env.CC_OUTPUT == "stdout") {
         console.log(json);
       } else {
-        json['repo_token'] = process.env.CODECLIMATE_REPO_TOKEN;
+        json['repo_token'] = repo_token
         client.postJson(json);
       }
     }
